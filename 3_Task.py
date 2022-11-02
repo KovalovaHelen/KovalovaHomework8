542ee73eadcbfd3f242b9f87ddd5bfcaee3ed552
KovalovaHelen <113236482+KovalovaHelen@users.noreply.github.com>
2022-11-02 13:06:51 +0200

    D.z_Тask3

--- 3_Task.py
+++ 3_Task.py
@@ -8,18 +8,36 @@
 5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
 """
 
-password = input("Введите пароль: ")
+password = input('Give me your password: ')
 
-if password == "qwerty" or password == "admin" or password == " ":
-    print("Оценка пароля 1")
-elif password.isupper() or password.islower() or password.isdigit():
-    print("Оценка пароля 2")
-elif password.isupper() or password.islower() and password.isdigit():
-    print("Оценка пароля 3")
-elif password.isupper() and password.islower() and password.isdigit():
-    print("Оценка пароля 4")
-elif len(password) > 8 and password.isupper() and password.islower() and password.isdigit():
-    print("Оценка пароля 5")
-else:
+has_digit = False
+has_upper = False
+has_lower = False
+has_spec = False
+
+
+if not password or password == 'admin' or password == 'qwerty':
+    print('Level 1')
     exit()
 
+for char in password:
+    if char.isdigit():
+        has_digit = True
+    elif char.isupper():
+        has_upper = True
+    elif char.islower():
+        has_lower = True
+    else:
+        has_spec = True
+
+correct_length = len(password) > 8
+total_result = has_digit + has_upper + has_lower + has_spec
+
+if total_result == 1:
+    print('Level 2')
+elif total_result == 2:
+    print('Level 3')
+elif total_result >= 3 and not correct_length:
+    print('Level 4')
+elif total_result == 4 and correct_length:
+    print('Level 5')
